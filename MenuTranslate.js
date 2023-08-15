366619eb28812a238751c17f5e104cc3f38142bd
karrycharon <karrycharon@qq.com>
2023-08-15 21:14:42 +0800

    init

---
+++ MenuTranslate.js
@@ -0,0 +1,177 @@
+export function applyMenuTranslation(T) {
+  translateAllText(document.querySelector(".litegraph"));
+  const viewHistoryButton = document.getElementById("comfy-view-history-button");
+  const viewQueueButton = document.getElementById("comfy-view-queue-button");
+
+  [viewHistoryButton, viewQueueButton].map((btn) => {
+    observeFactory(btn, (mutationsList, observer) => {
+      observer.disconnect();
+      for (let mutation of mutationsList) {
+        if (mutation.type === "childList") {
+          const translatedValue = T.Menu[mutation.target.textContent];
+          if (!translatedValue) continue;
+          mutation.target.innerText = translatedValue;
+        }
+      }
+      observer.observe(btn, { childList: true, attributes: true });
+    });
+  });
+
+  const comfySettingDialog = document.querySelector("#comfy-settings-dialog");
+
+  observeFactory(
+    comfySettingDialog.querySelector("tbody"),
+    handleComfySettingDialogObserver
+  );
+
+  observeFactory(
+    document.querySelector(".comfy-menu").querySelectorAll(".comfy-list")[0],
+    handleViewQueueComfyListObserver
+  );
+
+  observeFactory(
+    document.querySelector(".comfy-menu").querySelectorAll(".comfy-list")[1],
+    handleViewQueueComfyListObserver
+  );
+
+  observeFactory(
+    document.querySelector("body.litegraph"),
+    (mutationsList, observer) => {
+      for (let mutation of mutationsList) {
+        for (const node of mutation.addedNodes) {
+          translateAllText(node);
+        }
+      }
+    }
+  );
+
+  // search box
+  observeFactory(
+    document.querySelector(".litegraph"),
+    (mutationsList, observer) => {
+      if (observer.ob == undefined) {
+        observer.ob = [];
+      }
+      for (let mutation of mutationsList) {
+        if (mutation.removedNodes.length > 0 && observer.ob != undefined) {
+          for (let ob of observer.ob)
+            ob.disconnect();
+          observer.ob = [];
+          break;
+        }
+        for (const sb of mutation.addedNodes) {
+          var helper = sb.querySelector(".helper");
+          if (!helper) continue;
+          var ob = observeFactory(helper, (mutationsList, observer) => {
+            for (let mutation of mutationsList) {
+              for (const item of mutation.addedNodes) {
+                if (item.innerText in T.Nodes) {
+                  item.innerText = T.Nodes[item.innerText]["title"];
+                }
+              }
+            }
+          });
+          for (let item of helper.querySelectorAll(".lite-search-item")) {
+            if (item.innerText in T.Nodes) {
+              item.innerText = T.Nodes[item.innerText]["title"];
+            }
+          }
+          observer.ob.push(ob);
+        }
+      }
+    }
+  );
+
+  function handleViewQueueComfyListObserver(mutationsList) {
+    for (let mutation of mutationsList) {
+      replaceText(mutation.target);
+      if (mutation.type === "childList" && mutation.addedNodes.length > 0) {
+        for (const node of mutation.addedNodes) {
+          replaceText(node);
+        }
+      }
+    }
+  }
+
+  const translateSettingDialog = () => {
+    const comfySettingDialogAllElements = comfySettingDialog.querySelectorAll("*");
+
+    for (const ele of comfySettingDialogAllElements) {
+      let targetLangText = T.Menu[ele.innerText];
+      if (!targetLangText) {
+        if (ele.nodeName === "INPUT" && ele.type === "button") {
+          targetLangText = T.Menu[ele.value];
+          if (!targetLangText) continue;
+          ele.value = targetLangText;
+        }
+        continue;
+      }
+      replaceText(ele);
+    }
+  };
+
+  function handleComfySettingDialogObserver(mutationsList) {
+    for (let mutation of mutationsList) {
+      if (mutation.type === "childList" && mutation.addedNodes.length > 0) {
+        translateSettingDialog();
+      }
+    }
+  }
+
+  function translateAllText(node) {
+    const allElements = node.querySelectorAll("*");
+
+    for (const ele of allElements) {
+      let targetLangText = T.Menu[ele.innerText];
+      if (!targetLangText) {
+        if (ele.nodeName === "INPUT" && ele.type === "button") {
+          targetLangText = T.Menu[ele.value];
+          if (!targetLangText) continue;
+          ele.value = targetLangText;
+        }
+        continue;
+      }
+      replaceText(ele);
+    }
+  }
+
+  function replaceText(target) {
+    if (target?.childNodes.length > 1) {
+      for (const childNode of target.childNodes) {
+        replaceText(childNode);
+        const targetLangText =
+          childNode?.nodeType === Node.ELEMENT_NODE
+            ? T.Menu[childNode.innerText]
+            : T.Menu[childNode.nodeValue];
+        if (!targetLangText) continue;
+        if (childNode?.nodeType === Node.TEXT_NODE) {
+          childNode.nodeValue = targetLangText;
+        }
+        if (childNode?.nodeType === Node.ELEMENT_NODE) {
+          childNode.innerText = targetLangText;
+        }
+      }
+    } else {
+      if (target?.firstChild) {
+        replaceText(target.firstChild);
+      }
+      if (target?.firstChild?.nodeType === Node.TEXT_NODE) {
+        const targetLangText = T.Menu[target.firstChild.nodeValue];
+        if (!targetLangText) return;
+        target.innerText = targetLangText;
+      }
+    }
+  }
+}
+
+export function observeFactory(observeTarget, fn) {
+  const observer = new MutationObserver(function (mutationsList, observer) {
+    fn(mutationsList, observer);
+  });
+
+  observer.observe(observeTarget, {
+    childList: true,
+    attributes: true,
+  });
+  return observer;
+}
---
+++ __init__.py
@@ -0,0 +1,156 @@
+import os
+import json
+import sys
+import numpy as np
+import builtins
+import torch
+import shutil
+import hashlib
+import atexit
+import server
+import gc
+import execution
+import folder_paths
+from functools import lru_cache
+from aiohttp import web
+from pathlib import Path
+from PIL import Image
+from PIL.PngImagePlugin import PngInfo
+
+VERSION = "0.0.1"
+ADDON_NAME = "AIGODLIKE-COMFYUI-TRANSLATION"
+COMFY_PATH = Path(folder_paths.__file__).parent
+CUR_PATH = Path(__file__).parent
+
+
+def try_get_json(path: Path):
+    for coding in {"utf-8", "gbk"}:
+        try:
+            return json.loads(path.read_text(encoding=coding))
+        except Exception:
+            continue
+    return {}
+
+
+def get_nodes_translation(locale):
+    path = CUR_PATH.joinpath(locale, "Nodes")
+    if not path.exists():
+        path = CUR_PATH.joinpath("en_US")
+    if not path.exists():
+        return {}
+    translations = {}
+    for jpath in path.glob("*.json"):
+        translations.update(try_get_json(jpath))
+    return translations
+
+
+def get_category_translation(locale):
+    path = CUR_PATH.joinpath(locale, "NodeCategory.json")
+    if not path.exists():
+        path = CUR_PATH.joinpath("en_US", "NodeCategory.json")
+    if not path.exists():
+        return {}
+    return try_get_json(path)
+
+
+def get_menu_translation(locale):
+    path = CUR_PATH.joinpath(locale, "Menu.json")
+    if not path.exists():
+        path = CUR_PATH.joinpath("en_US", "Menu.json")
+    if not path.exists():
+        return {}
+    return try_get_json(path)
+
+
+@lru_cache
+def compile_translation(locale):
+    translations_path = CUR_PATH.joinpath(f"translations_{locale}.json")
+    if translations_path.exists():
+        return try_get_json(translations_path)
+    # Nodes
+    nodes_translation = get_nodes_translation(locale)
+    # NodeCategory
+    node_category_translation = get_category_translation(locale)
+    # Menus
+    menu_translation = get_menu_translation(locale)
+    # compile
+    json_data = json.dumps(obj={"Nodes": nodes_translation,
+                                "NodeCategory": node_category_translation,
+                                "Menu": menu_translation
+                                },
+                           indent=4,
+                           ensure_ascii=False)
+    translations_path.write_text(json_data, encoding="utf-8")
+    return json_data
+
+
+@server.PromptServer.instance.routes.post("/agl/get_translation")
+async def get_translation(request):
+    post = await request.post()
+    locale = post.get("locale", "en_US")
+    json_data = "{}"
+    try:
+        json_data = compile_translation(locale)
+    except Exception as e:
+        sys.stderr.write(f"agl/get_translation error: {e}\n")
+        sys.stderr.flush()
+    return web.Response(status=200, body=json_data)
+
+
+def rmtree(path: Path):
+    if path.is_file():
+        path.unlink()
+    elif path.is_dir():
+        for child in path.iterdir():
+            rmtree(child)
+        try:
+            path.rmdir()  # nas 的共享盘可能会有残留
+        except BaseException:
+            ...
+
+
+def register():
+    aigodlike_ext_path = COMFY_PATH.joinpath("web", "extensions", ADDON_NAME)
+    if aigodlike_ext_path.exists():
+        rmtree(aigodlike_ext_path)
+    aigodlike_ext_path.mkdir(parents=True, exist_ok=True)
+    # 复制所有js文件
+    for data in CUR_PATH.glob("*.js"):
+        shutil.copy(data, aigodlike_ext_path)
+    # shutil.copy(CUR_PATH.joinpath("main.js"), aigodlike_ext_path)
+
+    # combine jsons into one
+    # translations_path = CUR_PATH.joinpath("translations.json")
+    # translations = {}
+    # for data in CUR_PATH.glob("*.json"):
+    #     if data.name == "translations.json":
+    #         continue
+    #     try:
+    #         json_data = json.loads(data.read_text(encoding="utf-8"))
+    #     except UnicodeDecodeError:
+    #         json_data = json.loads(data.read_text(encoding="gbk"))
+    #     translations.update(json_data)
+    # translations_path.write_text(json.dumps(translations, indent=4, ensure_ascii=False), encoding="utf-8")
+    # 文件复制 translations.json main.js
+    # shutil.copy(translations_path, aigodlike_ext_path)
+
+
+def unregister():
+    # 移除缓存json
+    for data in CUR_PATH.glob("*.json"):
+        if not data.name.startswith("translations_"):
+            continue
+        data.unlink()
+
+    aigodlike_ext_path = COMFY_PATH.joinpath("web", "extensions", ADDON_NAME)
+    if not aigodlike_ext_path.exists():
+        return
+    try:
+        rmtree(aigodlike_ext_path)
+    except BaseException:
+        ...
+
+
+register()
+atexit.register(unregister)
+NODE_CLASS_MAPPINGS = {}
