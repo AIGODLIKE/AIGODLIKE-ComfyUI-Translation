75cf1649a3757a3f2521ff2bc98ca87e508a3756
karrycharon <karrycharon@qq.com>
2024-01-13 20:42:03 +0800

    fix switch locale

--- main.js
+++ main.js
@@ -19,6 +19,7 @@ export class TUtils {
 		localStorage[TUtils.LOCALE_ID_LAST] = localStorage.getItem(TUtils.LOCALE_ID) || "en-US";
 		localStorage[TUtils.LOCALE_ID] = locale;
 		// TUtils.syncTranslation();
+		location.reload();
 	}
 
 	static syncTranslation(OnFinished = () => { }) {
@@ -311,7 +312,6 @@ export class TUtils {
 					return;
 				if (localStorage[id] != undefined && value != localStorage[id]) {
 					TUtils.setLocale(value);
-					location.reload();
 				}
 				localStorage[id] = value;
 			},
@@ -395,8 +395,6 @@ const ext = {
 						localeLast = "en-US";
 					if (locale != localeLast) {
 						app.ui.settings.setSettingValue(TUtils.LOCALE_ID, localeLast);
-						TUtils.setLocale(localeLast);
-						location.reload();
 					}
 				},
 			}));
