8ee3268a5544684e3145ea50d10de50cb0481abe
KarryCharon <karrycharon@qq.com>
2024-02-21 09:00:39 +0800

    exclude lite-search-item-type translate; code refactor;

--- MenuTranslate.js
+++ MenuTranslate.js
@@ -1,9 +1,72 @@
+class TExe {
+  static T = null;
+
+  constructor() {
+    this.excludeClass = ["lite-search-item-type"];
+  }
+
+  tSkip(node) {
+    // 是否需要跳过翻译?
+    // 判断node.classList 是否包含 excludeClass中的一个
+    return this.excludeClass.some((cls) => node.classList?.contains(cls));
+  }
+
+  translateAllText(node) {
+    let T = this.T;
+    if (!T) return;
+    const allElements = node.querySelectorAll("*");
+
+    for (const ele of allElements) {
+      let targetLangText = T.Menu[ele.innerText];
+      if (!targetLangText) {
+        if (ele.nodeName === "INPUT" && ele.type === "button") {
+          targetLangText = T.Menu[ele.value];
+          if (!targetLangText) continue;
+          ele.value = targetLangText;
+        }
+        continue;
+      }
+      this.replaceText(ele);
+    }
+  }
+
+  replaceText(target) {
+    let T = this.T;
+    if (!T) return;
+    if (this.tSkip(target)) return;
+    if (target?.childNodes.length > 1) {
+      for (const childNode of target.childNodes) {
+        this.replaceText(childNode);
+        const targetLangText = childNode?.nodeType === Node.ELEMENT_NODE ? T.Menu[childNode.innerText] : T.Menu[childNode.nodeValue];
+        if (!targetLangText) continue;
+        if (childNode?.nodeType === Node.TEXT_NODE) {
+          childNode.nodeValue = targetLangText;
+        }
+        if (childNode?.nodeType === Node.ELEMENT_NODE) {
+          childNode.innerText = targetLangText;
+        }
+      }
+    } else {
+      if (target?.firstChild) {
+        this.replaceText(target.firstChild);
+      }
+      if (target?.firstChild?.nodeType === Node.TEXT_NODE) {
+        const targetLangText = T.Menu[target.firstChild.nodeValue];
+        if (!targetLangText) return;
+        target.innerText = targetLangText;
+      }
+    }
+  }
+}
+let texe = new TExe();
+
 export function applyMenuTranslation(T) {
-  translateAllText(document.querySelector(".litegraph"));
+  texe.T = T;
+  texe.translateAllText(document.querySelector(".litegraph"));
   for (let node of document.querySelectorAll(".comfy-modal"))
     observeFactory(node, (mutationsList, observer) => {
       for (let mutation of mutationsList) {
-        translateAllText(mutation.target);
+        texe.translateAllText(mutation.target);
       }
     });
 
@@ -26,87 +89,68 @@ export function applyMenuTranslation(T) {
 
   const comfySettingDialog = document.querySelector("#comfy-settings-dialog");
 
-  observeFactory(
-    comfySettingDialog.querySelector("tbody"),
-    handleComfySettingDialogObserver
-  );
+  observeFactory(comfySettingDialog.querySelector("tbody"), handleComfySettingDialogObserver);
 
-  observeFactory(
-    document.querySelector(".comfy-menu"),
-    handleViewQueueComfyListObserver
-  );
+  observeFactory(document.querySelector(".comfy-menu"), handleViewQueueComfyListObserver);
 
-  observeFactory(
-    document.querySelector(".comfy-menu").querySelectorAll(".comfy-list")[0],
-    handleViewQueueComfyListObserver
-  );
+  observeFactory(document.querySelector(".comfy-menu").querySelectorAll(".comfy-list")[0], handleViewQueueComfyListObserver);
 
-  observeFactory(
-    document.querySelector(".comfy-menu").querySelectorAll(".comfy-list")[1],
-    handleViewQueueComfyListObserver
-  );
+  observeFactory(document.querySelector(".comfy-menu").querySelectorAll(".comfy-list")[1], handleViewQueueComfyListObserver);
 
-  observeFactory(
-    document.querySelector("body.litegraph"),
-    (mutationsList, observer) => {
-      for (let mutation of mutationsList) {
-        for (const node of mutation.addedNodes) {
-          translateAllText(node);
-          if(node.classList.contains("comfy-modal")) {
-            observeFactory(node, (mutationsList, observer) => {
-              for (let mutation of mutationsList) {
-                translateAllText(mutation.target);
-              }
-            });
-          }
-        }
-      }
-    }
-  );
-
-  // search box
-  observeFactory(
-    document.querySelector(".litegraph"),
-    (mutationsList, observer) => {
-      if (observer.ob == undefined) {
-        observer.ob = [];
-      }
-      for (let mutation of mutationsList) {
-        if (mutation.removedNodes.length > 0 && observer.ob != undefined) {
-          for (let ob of observer.ob)
-            ob.disconnect();
-          observer.ob = [];
-          break;
-        }
-        for (const sb of mutation.addedNodes) {
-          var helper = sb.querySelector(".helper");
-          if (!helper) continue;
-          var ob = observeFactory(helper, (mutationsList, observer) => {
+  observeFactory(document.querySelector("body.litegraph"), (mutationsList, observer) => {
+    for (let mutation of mutationsList) {
+      for (const node of mutation.addedNodes) {
+        texe.translateAllText(node);
+        if (node.classList.contains("comfy-modal")) {
+          observeFactory(node, (mutationsList, observer) => {
             for (let mutation of mutationsList) {
-              for (const item of mutation.addedNodes) {
-                if (item.innerText in T.Nodes) {
-                  item.innerText = T.Nodes[item.innerText]["title"];
-                }
-              }
+              texe.translateAllText(mutation.target);
             }
           });
-          for (let item of helper.querySelectorAll(".lite-search-item")) {
-            if (item.innerText in T.Nodes) {
-              item.innerText = T.Nodes[item.innerText]["title"];
-            }
-          }
-          observer.ob.push(ob);
         }
       }
     }
-  );
+  });
+
+  // search box
+  observeFactory(document.querySelector(".litegraph"), (mutationsList, observer) => {
+    if (observer.ob == undefined) {
+      observer.ob = [];
+    }
+    for (let mutation of mutationsList) {
+      if (mutation.removedNodes.length > 0 && observer.ob != undefined) {
+        for (let ob of observer.ob) ob.disconnect();
+        observer.ob = [];
+        break;
+      }
+      for (const sb of mutation.addedNodes) {
+        var helper = sb.querySelector(".helper");
+        if (!helper) continue;
+        var ob = observeFactory(helper, (mutationsList, observer) => {
+          for (let mutation of mutationsList) {
+            for (const item of mutation.addedNodes) {
+              if (item.innerText in T.Nodes) {
+                item.innerText = T.Nodes[item.innerText]["title"];
+              }
+            }
+          }
+        });
+        for (let item of helper.querySelectorAll(".lite-search-item")) {
+          if (item.innerText in T.Nodes) {
+            item.innerText = T.Nodes[item.innerText]["title"];
+          }
+        }
+        observer.ob.push(ob);
+      }
+    }
+  });
 
   function handleViewQueueComfyListObserver(mutationsList) {
     for (let mutation of mutationsList) {
-      replaceText(mutation.target);
+      texe.replaceText(mutation.target);
       if (mutation.type === "childList" && mutation.addedNodes.length > 0) {
         for (const node of mutation.addedNodes) {
-          replaceText(node);
+          texe.replaceText(node);
         }
       }
     }
@@ -125,7 +169,7 @@ export function applyMenuTranslation(T) {
         }
         continue;
       }
-      replaceText(ele);
+      texe.replaceText(ele);
     }
   };
 
@@ -133,51 +177,6 @@ export function applyMenuTranslation(T) {
     for (let mutation of mutationsList) {
       if (mutation.type === "childList" && mutation.addedNodes.length > 0) {
         translateSettingDialog();
-      }
-    }
-  }
-
-  function translateAllText(node) {
-    const allElements = node.querySelectorAll("*");
-
-    for (const ele of allElements) {
-      let targetLangText = T.Menu[ele.innerText];
-      if (!targetLangText) {
-        if (ele.nodeName === "INPUT" && ele.type === "button") {
-          targetLangText = T.Menu[ele.value];
-          if (!targetLangText) continue;
-          ele.value = targetLangText;
-        }
-        continue;
-      }
-      replaceText(ele);
-    }
-  }
-
-  function replaceText(target) {
-    if (target?.childNodes.length > 1) {
-      for (const childNode of target.childNodes) {
-        replaceText(childNode);
-        const targetLangText =
-          childNode?.nodeType === Node.ELEMENT_NODE
-            ? T.Menu[childNode.innerText]
-            : T.Menu[childNode.nodeValue];
-        if (!targetLangText) continue;
-        if (childNode?.nodeType === Node.TEXT_NODE) {
-          childNode.nodeValue = targetLangText;
-        }
-        if (childNode?.nodeType === Node.ELEMENT_NODE) {
-          childNode.innerText = targetLangText;
-        }
-      }
-    } else {
-      if (target?.firstChild) {
-        replaceText(target.firstChild);
-      }
-      if (target?.firstChild?.nodeType === Node.TEXT_NODE) {
-        const targetLangText = T.Menu[target.firstChild.nodeValue];
-        if (!targetLangText) return;
-        target.innerText = targetLangText;
       }
     }
   }
